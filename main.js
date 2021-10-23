2c9ff119e6e5bf8a8761ad9079b69a378693b712
Aljaaiz <aljaaiz1@gmail.com>
2021-10-23 22:54:52 +0400

    added comment to Js file

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 let hamburger = document.getElementById("hamburger");
 let mobile_menu = document.querySelector(".mobile-links");
 
+// Add click functionality to hamburger
 hamburger.addEventListener("click", function () {
   mobile_menu.classList.add("active");
 });
 
+// select all li element in  the navbar
 let lis = document.querySelectorAll(".li-link");
+// looptthorough them to click on each link individually
 lis.forEach((li) => {
   li.addEventListener("click", function () {
+    //remove active class from menu
     mobile_menu.classList.remove("active");
   });
 });
