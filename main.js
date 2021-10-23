5214bde6883a5445a4387b5abf766acbc0e20a7c
Aljaaiz <aljaaiz1@gmail.com>
2021-10-23 22:43:15 +0400

    Adding js mobile menu close on link been  cliked

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ let hamburger = document.getElementById("hamburger");
 let mobile_menu = document.querySelector(".mobile-links");
 
 hamburger.addEventListener("click", function () {
-  mobile_menu.classList.toggle("active");
+  mobile_menu.classList.add("active");
+});
+
+let lis = document.querySelectorAll(".li-link");
+lis.forEach((li) => {
+  li.addEventListener("click", function () {
+    mobile_menu.classList.remove("active");
+  });
 });
